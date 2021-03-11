97e3be84ca5465873d3df3da58802925f5249477
Hya Viaje <72449952+ArHya@users.noreply.github.com>
2021-03-11 13:32:32 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  rootElement
+);
+
+// var randonNumber1 = 0;
+
+ReactDOM.render(document.getElementById("root"));
